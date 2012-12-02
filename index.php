c6b54537b889ff3d41a60584bfec55badcb0cce1
HesselvanApeldoorn <hesselva1@gmail.com>
2012-12-02 16:43:27 +0100

    Database connection is now working. Notifications now contain messages retrieved from the database. Modified CSS for home page notifications and for the library part.

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
-        <?php require_once 'templates/base.php' ?>
+        <?php require 'templates/base.php' ?>
         <link href="static/css/base.css" rel="stylesheet" type="text/css">
-    	<?php startblock('header') ?>
-    	Home
-    	<?php endblock() ?>
-    	<?php startblock('content') ?>
-    			<?php ?>
-		<?php endblock('content') ?>
+    	<?php startblock('header');
+    		echo "Home";
+    	endblock() ?>
+    	<?php startblock('content');
+    		echo " <div class='library'>";
+    			echo "Sample data";
+    		echo "</div>";
+    		echo "<div class='notifications'>";
+    			echo "<div class='notifyHeader'> <h2>Notifications</h2></div>";
+	    		$query = $con->prepare('select * from Notification');
+	       		$query->execute();
+				foreach($query as $notification) {
+					echo "<div class='notification'>";
+	            		echo $notification['message'];
+	            	echo "</div>";
+	            }
+            echo "</div>";
+
+		endblock() ?>
--- templates/base.php
+++ templates/base.php
@@ -1,9 +1,12 @@
-<?php require_once 'ti.php' ?>
-<!-- {
+<?php require 'ti.php' ?>
+<?php require '../../libConfig.php' ?>
+<?php
+try {
     $con = new PDO("mysql:dbname=$db;host=$host", $username, $password);
 } catch(PDOException $e) {
     echo $e->getMessage();
-} -->
+}
+?>
 <html>
     <head>
          <link href="../static/css/base.css" rel="stylesheet" type="text/css">
