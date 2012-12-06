0f787b3265d3b48b4b43709f717f3ce87a284c41
Hessel <=>
2012-12-06 11:25:17 +0100

    added a file with things we do after the prototype. Added CSS for tables. Modified tables in library to make use of this CSS

--- library.php
+++ library.php
@@ -9,15 +9,22 @@ endblock() ?>
         echo "<div class='blockContent'>";
             echo "<div class='personal_library'>";
                 echo "<h4>Personal library</h4>";
-                echo "<table border='1'>"; 
+                echo "<table>"; 
                     echo "<th> Name</th>";
                     echo "<th> Author</th>";
                     echo "<th> Description </th>";
                     echo "<th> ISBN(optional)</th>";
                     $query = $con->prepare('select * from Document');
                     $query->execute();
+                    $i=0;
                     foreach($query as $document) {
-                        echo "<tr><td><a href='book.php?{$document['document_name']}'>{$document['document_name']}</a></td>";
+                        $i = $i+1;
+                        $i=$i%2;
+                        $idvar = 'even';
+                        if ($i==0) {
+                            $idvar='odd';
+                        }
+                        echo "<tr id='$idvar' ><td><a href='book.php?{$document['document_name']}'>{$document['document_name']}</a></td>";
                         echo "<td>{$document['author']}</td>";
                         echo "<td>{$document['description']}</td>";
                         echo "<td>{$document['isbn']}</td>";
@@ -27,18 +34,25 @@ endblock() ?>
             echo "</div>";
             echo "<div class='loaning'>";
                 echo "<hr/><h4>Books you lent out</h4>";
-                echo "<table border='1'>"; 
+                echo "<table>"; 
                     echo "<th>Book</th>";
                     echo "<th>Lent from</th>";
                     echo "<th>Start date</th>";
                     echo "<th>end date</th>";
                     $query = $con->prepare('select * from Loaning');
                     $query->execute();
+                    $i=0;
                     foreach($query as $loaning) {
+                        $i = $i+1;
+                        $i=$i%2;
+                        $idvar = 'even';
+                        if ($i==0) {
+                            $idvar='odd';
+                        }
                         $query2 = $con->prepare("select * from Document where docID={$loaning['docID']}");
                         $query2->execute();
                         $docName = $query2->fetch();  
-                        echo "<tr><td>{$docName['document_name']}</td>";
+                        echo "<tr id='$idvar'><td>{$docName['document_name']}</td>";
                         echo "<td>{$loaning['fromUser']}</td>";
                         echo "<td>{$loaning['start_date']}</td>";
                         echo "<td>{$loaning['end_date']}</td></tr>";
@@ -46,28 +60,35 @@ endblock() ?>
                 echo "</table>";
 
                 echo "<hr/><h4>Books you borrowed</h4>";
-                echo "<table border='1'>"; 
+                echo "<table>"; 
                     echo "<th>Book</th>";
                     echo "<th>Lent from</th>";
                     echo "<th>Start date</th>";
                     echo "<th>end date</th>";
                     $query = $con->prepare('select * from Loaning');
                     $query->execute();
+                    $i=0;
                     foreach($query as $loaning) {
+                        $i = $i+1;
+                        $i=$i%2;
+                        $idvar = 'even';
+                        if ($i==0) {
+                            $idvar='odd';
+                        }
                         $query2 = $con->prepare("select * from Document where docID={$loaning['docID']}");
                         $query2->execute();
                         $docName = $query2->fetch();  
-                        echo "<tr><td>{$docName['document_name']}</td>";
+                        echo "<tr id='$idvar'><td>{$docName['document_name']}</td>";
                         echo "<td>{$loaning['fromUser']}</td>";
                         echo "<td>{$loaning['start_date']}</td>";
                         echo "<td>{$loaning['end_date']}</td></tr>";
                     }
                 echo "</table>";
             echo "</div>";
+            echo "<hr/><form>";
+                echo "<input type='submit' value='Upload new Document' />";
+            echo "</form";
         echo "</div>";
-        echo "<hr/><form>";
-            echo "<input type='submit' value='Upload new Document' />";
-        echo "</form";
     echo "</div>";
 
 endblock() ?>
