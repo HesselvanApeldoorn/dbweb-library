1512a78d76f6b4eafeb2895376503d297e2fb9a3
Hessel <=>
2012-12-12 12:28:48 +0100

    make borrowDocument send a notification and selectdate work

--- borrowDocument.php
+++ borrowDocument.php
@@ -1,5 +1,5 @@
 <?php require 'templates/base.php';
-			error_reporting(-1);
+                        error_reporting(-1);
             ini_set("display_errors", 1); ?>
 <link href="static/css/base.css" rel="stylesheet" type="text/css">
 <?php startblock('scripts') ?>
@@ -22,38 +22,43 @@
 endblock() ?>
 <?php startblock('content'); ?>
 
-	<?php
+        <?php
     echo "<div class='main'>";
         echo "<div class='blockHeader'><h2>Loanings</h2></div><br>";
         echo "<div class='blockContent'>";
-            if(!isset($_REQUEST['borrow'])) {
-
-                $sql = "select * from PaperDoc";
-                $query = $con->prepare($sql);
-                $query->execute();
-                echo "<h4>Document</h4>";
-                echo "<select name='document'>";
-                foreach($query as $book) {
-                        $sql = "select * from Document where docID=?";
-                        $query = $con->prepare($sql);
-                        $query->execute(array(str_replace('"','',$book['docID'])));
-                        $document = $query->fetch;
-                        echo "<option value='{$book['docID']}' >{$document['document_name']}</option>";
+            if ($_SERVER['REQUEST_METHOD']=='POST') {
+                if($_REQUEST['start'] >= $_REQUEST['end']) {
+                    echo "Start date: " . $_REQUEST['start'];
+                    echo "<br/>End date: ".$_REQUEST['end'];
+                    echo "<p>Your start date is after the end date.</p>";
+                    echo "<a href=''>Retry</a>"; 
+                    echo $_REQUEST['selectedDoc'];
+                } else {
+                    $sql = "insert into Notification values(?,?,?,?)";
+                    $query = $con->prepare($sql);
+                    $query->execute(array(40504,"sample@hotmail.com","Cagri has requested a loaning. Document: {$_REQUEST['selectedDoc']}. Requested start date: {$_REQUEST['start']}. Requested end date: {$_REQUEST['end']}.", date("Y-m-d")));
+                    header("location:library.php");                    
                 }
-                echo "</select>";
-                echo "<hr/>";
-               
-                echo "<form>";
-                    echo "<p style='float:left'>Start date: <input type='text' name='start' id='datepicker' value='".date("m/d/Y")."'/>";
-                    echo "<p style='float:left'>End date: <input type='text' name='end' id='datepicker2' value='".date("m/d/Y",strtotime("+7 day"))."'/>";
+            } else { # method is GET
+                echo "<form method='post'>";
+                    $sql = "select * from PaperDoc";
+                    $query = $con->prepare($sql);
+                    $query->execute();
+                    echo "<h4>Document</h4>";
+                    echo "<select name='selectedDoc'>";
+                    foreach($query as $book) {
+                            $sql = "select * from Document where docID=?";
+                            $query2 = $con->prepare($sql);
+                            $query2->execute(array(str_replace('"','',$book['docID'])));
+                            $document = $query2->fetch();
+                            echo "<option name='selectedDoc' value='{$document['document_name']}' >{$document['document_name']}</option>";
+                    }
+                    echo "</select>";
+                    echo "<hr/>";
+                    echo "<p style='float:left'>Start date: <input type='date' name='start' id='datepicker' value='".date("m/d/Y")."'/>";
+                    echo "<p style='float:left'>End date: <input type='date' name='end' id='datepicker2' value='".date("m/d/Y",strtotime("+7 day"))."'/>";
                     echo "<input type='submit' name='borrow' value='Borrow' />";
                 echo "</form";
-            } else {
-                echo "Start date: " . $_REQUEST['start'];
-                echo "<br/>End date: ".$_REQUEST['end'];
-                if($_REQUEST['start'] >= $_REQUEST['end']) {
-                    echo "<p>Your start date is after the end date.</p>";
-                }
             }
         echo "</div>";  
     echo "</div>";
