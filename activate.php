6419714c166afa6bbbd7e3c513017537debeb11c
Mark Hoekstra <markhoekie@gmail.com>
2012-12-20 14:25:22 +0100

    Make register.php and activate.php

---
+++ activate.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <?php
+            error_reporting(-1);
+            ini_set("display_errors", 1);
+            session_start();
+            require_once("../../libConfig.php"); //$db=databaseName, $host=hostName, $user=userName, $pass=password.
+            if(!isset($_SESSION['confirmed'])) {
+                header("location:login.php");
+            }
+        ?>
+        <link href="static/css/base.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <?php 
+            if($_SESSION['confirmed']==false && $_SESSION['email']==$_GET['email'] && $_SESSION['confirm_code']==$_GET['confirm_code']) {
+                $_SESSION['confirmed'] = true;
+                echo "Your account has been activated succesfully! <a href=login.php>Login</a>";
+            } else {
+                echo "You've already confirmed your account";
+            }
+        ?>
+    </body>
+</html>
---
+++ register.php
@@ -0,0 +1,82 @@
+<html>
+    <head>
+        <?php
+            error_reporting(-1);
+            ini_set("display_errors", 1);
+            session_start();
+            require_once("../../libConfig.php"); //$db=databaseName, $host=hostName, $user=userName, $pass=password.
+        ?>
+        <link href="static/css/base.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <?php 
+            try { //Setting up the database connection
+                $con = new PDO("mysql:dbname=$db;host=$host", $username, $password);
+            } catch (PDOException $e) {
+                echo "Could not connect to database.";
+                die();
+            }
+            if($_SERVER['REQUEST_METHOD']=='POST') {
+                 if(!isset($_REQUEST['user_name']) || !isset($_REQUEST['password']) || !isset($_REQUEST['email']) || $_REQUEST['email']==''|| $_REQUEST['user_name']=='' || $_REQUEST['password']=='') {
+                    echo "All the fields are required, <a href='register.php'> retry</a>";
+                } else {
+                    $sql = "select count(*) from User where email=?";
+                    $q = $con->prepare($sql);
+                    $q->execute(array($_REQUEST['email']));
+                    if($q->fetchColumn()!=0) {
+                       echo "Email already exists, <a href='register.php'> retry</a>";
+                    } else {
+                        if(!filter_var($_REQUEST['email'], FILTER_VALIDATE_EMAIL)) {
+                            echo "Invalid email address, <a href='register.php'> retry</a>";
+                        } else {
+                            $sql = "INSERT INTO User (email,user_name,password) VALUES (?,?,?)";
+                            $q = $con->prepare($sql);
+                            $q->execute(array($_REQUEST['email'],$_REQUEST['user_name'], hash("sha512",$_REQUEST['password'])));
+                            $confirm_code = md5(uniqid(rand()));
+                            $_SESSION['confirm_code'] = $confirm_code;
+                            $_SESSION['email'] = $_REQUEST['email'];
+                            $_SESSION['confirmed'] = false;
+                            send_mail($_REQUEST['email'], $confirm_code);
+                            echo "A confirmation link has been sent to your email account";
+                        }
+                            
+                    }
+                }
+            } else {
+               echo "Fill in your email, username and password: <br/>";
+                echo "<form method = 'post'>";
+                echo "Email: <input type = 'text' name = 'email'/> <br/>";
+                echo "Username: <input type = 'text' name = 'user_name'/> <br/>";
+                echo "Password: <input type = 'password' name = 'password'/> <br/>";
+                echo "<input type = 'submit' name = 'submit' value = 'Register'> <br/>
+                    </form>";
+            }
+        ?>
+    </body>
+</html>
+
+<?php
+function send_mail($email, $confirm_code) {
+     // Send the email:
+    $message = " To activate your account, please click on this link:\n\n";
+    $url = str_replace(curPageName(),"",curPageUrl());
+    
+    $message .= $url . 'activate.php?email=' . urlencode($email) . "&confirm_code=$confirm_code";
+    mail($email, 'Registration Confirmation', $message, 'From:no-reply@libDev.com');
+
+}
+
+function curPageURL() {
+ $pageURL = 'http://';
+ if ($_SERVER["SERVER_PORT"] != "80") {
+  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+ } else {
+  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+ }
+ return $pageURL;
+}
+
+function curPageName() {
+ return substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
+}
+?>
