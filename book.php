c2be6cc9441df14a5ff582292cea36793839ebce
Mark Hoekstra <markhoekie@gmail.com>
2013-01-10 14:01:12 +0100

    Improve code, Fix ownBook( change), change book quality dropdown.

--- book.php
+++ book.php
@@ -78,24 +78,22 @@ if ($_SERVER['REQUEST_METHOD']=='POST' and isset($_REQUEST['discard'])) {
         echo "<a href='index.php'>Home</a> &raquo; <a href='personalLibrary.php'>Personal Library</a> &raquo; Document";
     endblock();
     startblock('content');
-        $sql = "select * from Document left join PaperDoc on Document.docID=PaperDoc.docID
+        $sql = "select *, PaperDoc.email as PDemail, ElectronicDocCopies.email as EDemail from Document left join PaperDoc on Document.docID=PaperDoc.docID
                 left join ElectronicDocCopies on Document.docID=ElectronicDocCopies.docID where Document.docID=?";
         $query = $con->prepare($sql);
         $query->execute(array($_GET['book']));
         echo "<div class='main'>";
             echo "<div class='blockHeader'> <h2>Document: {$book['document_name']}</h2></div>";
             echo "<div class='blockContent'>";
-                if (False) {
+                if ($query->rowCount()==0) {
                     echo "Document not found";
                 } else {
                     $ownBook = False;
                     foreach($query as $bookRow) {
-                        if ($bookRow['email']==$_SESSION['email']) {
+                        if ($bookRow['EDemail']==$_SESSION['email'] || $bookRow['PDemail']==$_SESSION['email']) {
                             $ownBook = True;
                         }
                     }
-                    $sql = "select * from Document left join PaperDoc on Document.docID=PaperDoc.docID
-                    left join ElectronicDocCopies on Document.docID=ElectronicDocCopies.docID where Document.docID=?";
                     $query = $con->prepare($sql);
                     $query->execute(array($_GET['book']));
                     $book = $query->fetch();
@@ -103,35 +101,30 @@ if ($_SERVER['REQUEST_METHOD']=='POST' and isset($_REQUEST['discard'])) {
                         echo "You are not allowed to view this document.";
                     } else {
                         echo "<form method='post'>";
-                            echo "<hr/>";
-                            echo "<h4>Name</h4>";
+                            echo "<h4>Name<hr/></h4>";
                             if ($ownBook) {
                                 echo "<input type='text' name='document_name' value='{$book['document_name']}'/>";
                             } else {
                                 echo $book['document_name'];
                             }
-                            echo "<hr/>";
-                            echo "<h4>Author</h4>";
+                            echo "<h4>Author<hr/></h4>";
                             if ($ownBook) {
                                 echo "<input type='text' name='author' value='{$book['author']}'/>";
                             } else {
                                 echo $book['author'];
                             }
-                            echo "<hr/>";
-                            echo "<h4>Description</h4>";
+                            echo "<h4>Description<hr/></h4>";
                             if ($ownBook) {
                                 echo "<textarea name='description'>{$book['description']}</textarea>";
                             } else {
                                 echo $book['description'];
                             }
-                            echo "<hr/>";
-                            echo "<h4>ISBN</h4>";
+                            echo "<h4>ISBN<hr/></h4>";
                             if ($ownBook) {
                                 echo "<input type='text' name='isbn' value='{$book['isbn']}'/>";
                             } else {
                                 echo $book['isbn'];
                             }
-                            echo "<hr/>";
                             $sql = "select * from PaperDoc where docID =?";
                             $query = $con->prepare($sql);
                             $query->execute(array($_GET['book']));
@@ -139,7 +132,7 @@ if ($_SERVER['REQUEST_METHOD']=='POST' and isset($_REQUEST['discard'])) {
                             $sql = "select * from DocCategory where docID=?";
                             $query = $con->prepare($sql);
                             $query->execute(array($_GET['book']));
-                            echo "<h4>Categories</h4>";
+                            echo "<h4>Categories<hr/></h4>";
                             $categories = array("action","fantasy","fiction","romance", "comedy","adventure", "non-fiction", "education");
                             echo "<table border='0'>";
                                 echo "<tr>";
@@ -155,42 +148,47 @@ if ($_SERVER['REQUEST_METHOD']=='POST' and isset($_REQUEST['discard'])) {
                                             }
                                         }
                                         if($checked) {
-                                            if ($ownBook) { #  If the book is your own, you are allowed to modify the checkboxes
-                                                echo "<td><input type = 'checkbox' checked name = 'category[]' value='{$categories[$i]}' />$categories[$i]</td>";
-                                            } else {
-                                                echo "<td><input type = 'checkbox' checked name = 'category[]' value='{$categories[$i]}' disabled />$categories[$i]</td>";
-                                            }
+                                          $checkString = 'checked';
                                         } else {
-                                            if($ownBook) {
-                                                echo "<td><input type = 'checkbox' name = 'category[]' value='{$categories[$i]}' />$categories[$i]</td>";
-                                            } else {
-                                                echo "<td><input type = 'checkbox' name = 'category[]' value='{$categories[$i]}' disabled />$categories[$i]</td>";
-                                            }
+                                          $checkString = '';
                                         }
+                                        if($ownBook) { #If the book is your own, you are allowed to modify the checkboxes
+                                            $ownBookString = '';
+                                        } else {
+                                            $ownBookString = 'disabled';
+                                        }
+                                        echo "<td><input type = 'checkbox' $checkString name = 'category[]' value='{$categories[$i]}' $ownBookString />$categories[$i]</td>";
                                     }
                                 echo "</tr>";
                             echo "</table>";
                             if ($ownBook) {
-                                echo "<hr/>";
-                                echo "<h4>Visibility</h4>";
+                                echo "<h4>Visibility<hr/></h4>";
                                 if ($book['visible']) {
-                                    echo "<input type = 'radio' checked name = 'visible' value='visible' >visible<br/>";
-                                    echo "<input type = 'radio' name = 'visible' value='notvisible' >not visible";
+                                    $visChecked = 'checked';
+                                    $notVisChecked = '';
                                 } else {
-                                    echo "<input type = 'radio' name = 'visible' value='visible' >visible<br/>";
-                                    echo "<input type = 'radio' checked name = 'visible' value='notvisible' >not visible";
+                                    $visChecked = '';
+                                    $notVisChecked = 'checked';
                                 }
+                                echo "<input type = 'radio' $visChecked name = 'visible' value='visible' >visible<br/>";
+                                echo "<input type = 'radio' $notVisChecked name = 'visible' value='notvisible' >not visible";
                             }
-                            echo "<hr/>";
                             if (isset($paperDoc['docID'])) { #paper doc
-                                echo "<h4>Quality</h4>";
-                                echo "Current: ".$paperDoc['state']."</br>";
+                                echo "<h4>Quality<hr/></h4>";
+                                echo "Current: ";
+                                $options = array("","","","");
+                                $values = array("new","good","decent","poor");
+                                for($i=0;$i<sizeof($options);$i++) {
+                                    if($paperDoc['state']==$values[$i]) {
+                                        $options[$i] =  "selected='selected'";
+                                    }
+                                }
                                 if ($ownBook) {
                                     echo "<select name='state'>";
-                                        echo "<option value='new'>new</option>";
-                                        echo "<option value='good'>good</option>";
-                                        echo "<option value='decent'>decent</option>";
-                                        echo "<option value='poor'>poor</option>";
+                                        echo "<option $options[0] value='new'>new</option>";
+                                        echo "<option $options[1] value='good'>good</option>";
+                                        echo "<option $options[2] value='decent'>decent</option>";
+                                        echo "<option $options[3] value='poor'>poor</option>";
                                     echo "</select>";
                                 }
                             } else { #electronic doc
@@ -201,23 +199,22 @@ if ($_SERVER['REQUEST_METHOD']=='POST' and isset($_REQUEST['discard'])) {
                                 if ($ownBook) {
                                     echo "<h4>Distributable</h4>";
                                     if ($electronicDoc['distributable']) {
-                                        echo "<input type = 'radio' checked name = 'distributable'value='distributable' />distributable<br/>";
-                                        echo "<input type = 'radio' name = 'distributable' value='notdistributable' />not distributable";
+                                        $distChecked = 'checked';
+                                        $notDistChecked = '';
                                     } else {
-                                        echo "<input type = 'radio' name = 'distributable' value='distributable' />distributable<br/>";
-                                        echo "<input type = 'radio' checked name = 'distributable' value='notdistributable' />not distributable";
+                                        $distChecked = '';
+                                        $notDistChecked = 'checked';
                                     }
-                                    echo "<hr/>";
+                                    echo "<input type = 'radio' $distChecked name = 'distributable'value='distributable' />distributable<br/>";
+                                    echo "<input type = 'radio' $notDistChecked name = 'distributable' value='notdistributable' />not distributable";
                                 }
-                                echo "<h4>Extension</h4>";
+                                echo "<h4>Extension<hr/></h4>";
                                 echo $electronicDoc['extension'];
-                                echo "<hr/>";
-                                echo "<h4>Size</h4>";
+                                echo "<h4>Size<hr/></h4>";
                                 echo $electronicDoc['size']." Bytes";
                             }
                             if (!isset($paperDoc['docID'])) { #electronic doc, downloadble
-                                echo "<hr/>";
-                                echo "<h4>Content</h4>";
+                                echo "<h4>Content<hr/></h4>";
                                 $id=$_GET['book'];
                                 if ($ownBook or $electronicDoc['distributable']) {
                                     echo "Download: <a href='download.php?id=".$id."'>{$book['document_name']}</a>";
